b39192a543a6d1f16b31dce6023c28d1d7378eb7
Murage-Ivy <ivymurage2000@gmail.com>
2023-02-25 14:05:20 +0300

    Add a sign up option on the sign up form

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -4,13 +4,15 @@ import "./SignUp.css";
 function SignUp() {
   return (
     <div className="sign-up-group">
-
       <form className="signup-form-group">
-      <h2>Welcome to Chattis</h2>
+        <h2>Welcome to Chattis</h2>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <input type="password" placeholder="Confirm Password" />
         <input type="submit" value="Sign Up" className="signup-btn" />
+        <footer>
+          Already Have an Account? <span className="login-option">Log In</span>
+        </footer>
       </form>
     </div>
   );
